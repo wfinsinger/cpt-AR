8b8863eb2159a3216f38db530b58e94357df7f85
Walter Finsinger <walter.finsinger@univ-montp2.fr>
2016-06-09 21:31:05 +0200

    Delete cpt_AR_example.r

--- cpt_AR_example.r
+++
@@ -1,9 +0,0 @@
-# Load cpt source file
-source('CPT_v1.r')
-
-# Load record
-xample = read.csv("CPT_example.csv", header=T)
-
-
-# Run Change-point analysis function
-proxy.cpt(Data=xample, Name="xample")
